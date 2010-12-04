82d34e89bcd5b2b79aba0fc16730cd7193e1a562
Tyler C <tyler@chacha102.com>
2010-12-04 14:22:42 -0800

    Added in the build time for better debugging

--- js/Triton.js
+++ js/Triton.js
@@ -7,6 +7,7 @@ window.Triton = {
         "Lucas Jones"
     ],
     "updated" : '<?php echo date("n.j.Y"); ?>',
+    "updated_time" : '<?php echo date("G:i:s"); ?>',
     "copyright" : "2010",
     "debug" : true
 };
@@ -15,4 +16,5 @@ window.Triton = {
 if(window.Triton.updated.indexOf("<") == 0){
     d = new Date();
     window.Triton.updated = (d.getMonth() + 1) + "." + d.getDate() + "." + d.getFullYear();
+    window.Triton.updated_time = d.getUTCHours() + ":" + d.getUTCMinutes() + ":" + d.getSeconds();
 }
